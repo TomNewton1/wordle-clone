e12ce620ead9e817a8148e95e05f58d71cfb8da7
Tom Newton <tomnewtonx@gmail.com>
2025-03-13 18:45:48 +0000

    Add @testing-library/jest-dom for improved testing assertions

--- src/tests/WordGrid.test.tsx
+++ src/tests/WordGrid.test.tsx
@@ -2,6 +2,7 @@ import { WordGrid } from "../components/WordGrid";
 import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 import { fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 test("WordGrid component renders", () => {
   render(<WordGrid />);
